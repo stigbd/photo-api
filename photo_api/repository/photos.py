518be427f7135a89c79b958df14f1beea38cf53f
Stig B. Dørmænen <stigbd@gmail.com>
2023-09-13 10:48:21 +0200

    Fix typo in db-name

--- photo_api/repository/photos.py
+++ photo_api/repository/photos.py
@@ -10,7 +10,7 @@ from ..models import Photo
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
 POSTGRES_SSLMODE = os.getenv("POSTGRES_SSLMODE", "prefer")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "phtoto_api")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "photo_api")
 POSTGRES_SCHEMA = os.getenv("POSTGRES_SCHEMA", "public")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "example")
